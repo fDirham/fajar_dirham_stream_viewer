21d1d28777a83e2ac9b8499d6e882c8ad3463ea1
FBD <fajarletters@gmail.com>
2025-11-11 22:57:40 -0800

    added no event stream

--- src/model/EventStreamMap.ts
+++ src/model/EventStreamMap.ts
@@ -2,9 +2,10 @@ import type { AppEventStream } from "./AppEventStream";
 import { MockEventStream } from "./MockEventStream";
 
 export const EventStreamMap: {
-  [id: string]: { eventStream: AppEventStream; label: string };
+  [id: string]: { eventStream: AppEventStream | null; label: string };
 } = {
   A: { eventStream: new MockEventStream("A"), label: "Mock Event Stream A" },
   B: { eventStream: new MockEventStream("B"), label: "Mock Event Stream B" },
   C: { eventStream: new MockEventStream("C"), label: "Mock Event Stream C" },
+  none: { eventStream: null, label: "None" },
 };
